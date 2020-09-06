bfced6db938b5fa6c8de0834de775a698ffe90d8
Parv Garg <parv3213@gmail.com>
2020-09-06 17:28:43 +0530

    challenge complete

---
+++ Interview Preparation Kit/Arrays/Arrays: Left Rotation.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+	inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+	inputString = inputString
+		.replace(/\s*$/, "")
+		.split("\n")
+		.map((str) => str.replace(/\s*$/, ""));
+
+	main();
+});
+
+function readLine() {
+	return inputString[currentLine++];
+}
+
+// Complete the rotLeft function below.
+function rotLeft(a, d) {
+	for (var i = 0; i < d; i++) {
+		var temp = a[a.length - 1];
+		var j = 0;
+		a[a.length - 1] = a[0];
+		a[j] = a[j + 1];
+		for (var j_1 = 1; j_1 < a.length - 2; j_1++) {
+			a[j_1] = a[j_1 + 1];
+		}
+		a[a.length - 2] = temp;
+	}
+	return a;
+}
+
+function main() {
+	const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+	const nd = readLine().split(" ");
+
+	const n = parseInt(nd[0], 10);
+
+	const d = parseInt(nd[1], 10);
+
+	const a = readLine()
+		.split(" ")
+		.map((aTemp) => parseInt(aTemp, 10));
+
+	const result = rotLeft(a, d);
+
+	ws.write(result.join(" ") + "\n");
+
+	ws.end();
+}
